2a25a00762bb9c8e6b7ac815ce907c950e71b270
Christophe Liu <lsmgeb89@gmail.com>
2016-09-29 00:50:21 -0500

    [CS 6380][project_1] Handle file input

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,31 @@
+#include <fstream>
+#include <iostream>
 #include "HSAlgo.h"
+#include "log_util.h"
 
 int main(int argc, char* argv[]) {
-  std::vector<std::size_t> ids{2, 6, 9, 5, 3, 7, 8};
-  HS::HSAlgo test(ids);
-  test.Run();
+  std::string line;
+  std::ifstream test_file(argv[1]);
+  std::vector<std::size_t> ids;
+
+  if (argc != 2) {
+    std::cerr << "Wrong parameter number: " << argc - 1 << std::endl;
+    return -1;
+  }
+
+  if (!test_file.is_open()) {
+    std::cerr << "Wrong test file path: " << argv[1] << std::endl;
+    return -1;
+  }
+
+  while (std::getline(test_file, line)) {
+    if (IsNumber(line)) {
+      ids.push_back(std::stoul(line));
+    }
+  }
+  test_file.close();
+
+  HS::HSAlgo hs_algorithm(ids);
+  hs_algorithm.Run();
   return 0;
 }
--- src/utils/log_util.h
+++ src/utils/log_util.h
@@ -15,4 +15,9 @@ template <typename T> inline std::lock_guard<T> lock_with(T &mutex) {
   return { mutex, std::adopt_lock };
 }
 
+inline bool IsNumber(const std::string& str) {
+  return (!str.empty() && std::find_if(str.begin(), str.end(),
+          [](char c) { return !std::isdigit(c); }) == str.end());
+}
+
 #endif // HS_LEADER_LOG_UTIL_H_
